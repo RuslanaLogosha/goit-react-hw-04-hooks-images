aacee624bfb2cb5fae6200aa8e3ab60a40e1eec6
Ruslana Logosha <ruslanalogosha@gmail.com>
2020-12-21 12:29:02 +0200

    Update pixabay-api.js

--- src/services/pixabay-api.js
+++ src/services/pixabay-api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 import { get } from 'react-scroll/modules/mixins/scroller';
 
-// axios.defaults.baseURL = 'https://pixabay.com/api/';
-// axios.defaults.headers.common['Authorization'] = '18957101-8b16af6433f6d6830b4844a72';
-
+const url = 'https://pixabay.com/api/';
+const key = '18957101-8b16af6433f6d6830b4844a72';
 const image_type = 'photo';
 const orientation = 'horizontal';
 
@@ -11,13 +10,13 @@ async function fetchImages(requestKey, page) {
   try {
     return await axios({
       method: get,
-      url: 'https://pixabay.com/api/',
+      url: url,
       params: {
         q: requestKey,
-        page: page,
-        key: '18957101-8b16af6433f6d6830b4844a72',
-        image_type: image_type,
-        orientation: orientation,
+        page,
+        key,
+        image_type,
+        orientation,
         per_page: 12,
       },
     });
@@ -29,7 +28,3 @@ async function fetchImages(requestKey, page) {
 const api = { fetchImages };
 
 export default api;
-
-// const API_KEY = '18957101-8b16af6433f6d6830b4844a72';
-// const BASE_URL = 'https://pixabay.com/api/';
-// const url = `${BASE_URL}?q=${requestKey}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
